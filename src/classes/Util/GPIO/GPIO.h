f85cccce54283bceeae399d051556a0eede4e157
kuang <snowy-shadow@github.com>
2024-01-14 16:18:31 -0600

    inital

---
+++ src/classes/Util/GPIO/GPIO.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<sstream>
+#include<cstdlib>
+#include<cstdio>
+#include<fcntl.h>
+#include<unistd.h>
+#include<sys/epoll.h>
+#include<pthread.h>
+
+class GPIO
+{
+
+};
---
+++ src/main.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Kuang on 2024-01-14.
+//
