190bdb3982bd38d2d5449819d080f5a3b3d397e1
kuang <snowy-shadow@github.com>
2024-02-04 13:50:56 -0600

    gpiod test code line toggle

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -1,15 +1,31 @@
 #include <Core.h>
 #include <iostream>
+#include <thread>
 
 #include <gpiod.hpp>
 
 int main()
 {
-	gpiod::chip Chip{ "gpiochip0" };
+    const std::filesystem::path ChipPath{"/dev/gpiochip0"};
+    const gpiod::line::offset LineOffset{5};
 
-	auto Request = Chip.prepare_request();
+    auto LineValue = gpiod::line::value::ACTIVE;
+    auto LineRequest = gpiod::chip{ChipPath}.prepare_request()
+            .set_consumer("toggle-line-value").add_line_settings(
+                    LineOffset,
+                    gpiod::line_settings().set_direction(
+                            gpiod::line::direction::OUTPUT))
+            .do_request();
 
-	Request.add_line_settings()
+    while(true)
+    {
+        std::cout << LineOffset << "=" << LineValue << ::std::endl;
+
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        LineValue = LineValue == gpiod::line::value::ACTIVE ?
+                gpiod::line::value::INACTIVE : gpiod::line::value::ACTIVE;
+        request.set_value(LineOffset, LineValue);
+    }
 
 	return 0;
 }
