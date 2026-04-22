aaa6d042fda10df9d1b7eb3ef0581e92b05f3949
Luke Marks <Lukemarks8@outlook.com>
2026-04-22 17:15:45 -0600

    Fixed syntax and dependency issues for service. Also created folder to store test scripts, and added eps script there

--- kubos/apis/clyde-3g-eps-api/src/telemetry/motherboard.rs
+++ kubos/apis/clyde-3g-eps-api/src/telemetry/motherboard.rs
@@ -32,9 +32,9 @@ make_telemetry!(
     /// VBCR1 - Voltage feeding BRC1 (V)
     VoltageFeedingBcr1 => {vec![0xE1, 0x10], |d| (0.032_253_7 * d) - 0.051_236_678},
     /// IBCR1A - Current BCR1, Connector SA1A (A)
-    CurrentBcr1Sa1a => {vec![0xE1, 0x14], |d| (0.0009946357 * d)},
+    CurrentBcr1Sa1a => {vec![0xE1, 0x14], |d| 0.0009946357 * d},
     /// IBCR1B - Current BCR1, Connection SA1B (A)
-    CurrentBcr1Sa1b => {vec![0xE1, 0x15], |d| (0.0009751609 * d)},
+    CurrentBcr1Sa1b => {vec![0xE1, 0x15], |d| 0.0009751609 * d},
     /// TBCR1A - Array Temp, Connector SA1A (*C)
     ArrayTempSa1a => {vec![0xE1, 0x18], |d| (0.356_573_44 * d) - 273.884_02},
     /// TBCR1B - Array Temp, Connector SA1B (*C)
@@ -47,9 +47,9 @@ make_telemetry!(
     /// VBCR2 - Voltage Feeding BCR2 (V)
     VoltageFeedingBcr2 => {vec![0xE1, 0x20], |d| (0.032_096_106 * d) + 0.056_830_684},
     /// IBCR2A - Current BCR2, Connection SA2A (A)
-    CurrentBcr2Sa2a => {vec![0xE1, 0x24], |d| (0.0010023357 * d)},
+    CurrentBcr2Sa2a => {vec![0xE1, 0x24], |d| 0.0010023357 * d},
     /// IBCR2B - Current BCR2, Connector SA2B (A)
-    CurrentBcr2Sa2b => {vec![0xE1, 0x25], |d| (0.0008085809 * d)},
+    CurrentBcr2Sa2b => {vec![0xE1, 0x25], |d| 0.0008085809 * d},
     /// TBCR2A - Array Temp, Connector SA2A (*C)
     ArrayTempSa2a => {vec![0xE1, 0x28], |d| (0.356_573_44 * d) - 273.884_02},
     /// TBCR2B - Array Temp, Connector SA2B (*C)
@@ -62,9 +62,9 @@ make_telemetry!(
     /// VBCR3 - Voltage Feeding BCR3 (V)
     VoltageFeedingBcr3 => {vec![0xE1, 0x30], |d| (0.010_021_041 * d) + 0.019_172_526},
     /// IBCR3A - Current BCR3, Connection SA3A (A)
-    CurrentBcr3Sa3a => {vec![0xE1, 0x34], |d| (0.0009819639 * d)},
+    CurrentBcr3Sa3a => {vec![0xE1, 0x34], |d| 0.0009819639 * d},
     /// IBCR3B - Current BCR3, Connector SA3B (A)
-    CurrentBcr3Sa3b => {vec![0xE1, 0x35], |d| (0.0009760956 * d)},
+    CurrentBcr3Sa3b => {vec![0xE1, 0x35], |d| 0.0009760956 * d},
     /// TBCR3A - Array Temp, Connector SA3A (*C)
     ArrayTempSa3a => {vec![0xE1, 0x38], |d| (0.356_573_44 * d) - 273.884_02},
     /// TBCR3B - Array Temp, Connector SA3B (*C)
@@ -84,62 +84,62 @@ make_telemetry!(
     CurrentDraw5V => {vec![0xE2, 0x15], |d| 0.001_327_547 * d},
 
     /// IPCM12V - Output Current of 12V bus (mA)
-    OutputCurrent12V => {vec![0xE2, 0x34], |d| (0.0021358945 * d)},
+    OutputCurrent12V => {vec![0xE2, 0x34], |d| 0.0021358945 * d},
     /// VPCM12V - Output Voltage of 12V bus (V)
     OutputVoltage12V => {vec![0xE2, 0x30], |d| 0.013_468_447 * d},
     /// IPCMBATV - Output Current of Battery Bus (mA)
-    OutputCurrentBattery => {vec![0xE2, 0x24], |d| (0.0064585469 * d)},
+    OutputCurrentBattery => {vec![0xE2, 0x24], |d| 0.0064585469 * d},
     /// VPCMBATV - Output Voltage of Battery Bus (V)
     OutputVoltageBattery => {vec![0xE2, 0x20], |d| 0.000_901_295_7 * d},
     /// IPCM5V - Output Current of 5V bus (mA)
-    OutputCurrent5V => {vec![0xE2, 0x14], |d| (0.0073345273 * d)},
+    OutputCurrent5V => {vec![0xE2, 0x14], |d| 0.0073345273 * d},
     /// VPCM5V - Output Voltage of 5V bus (V)
     OutputVoltage5V => {vec![0xE2, 0x10], |d| 0.005_846_589 * d},
     /// IPCM3V3 - Output Current of 3.3V Bus (mA)
-    OutputCurrent33V => {vec![0xE2, 0x04], |d| (0.0070599 * d)},
+    OutputCurrent33V => {vec![0xE2, 0x04], |d| 0.0070599 * d},
     /// VPCM3V3 - Output Voltage of 3.3V Bus (V)
     OutputVoltage33V => {vec![0xE2, 0x00], |d| 0.004_288_677 * d},
 
     /// VSW1 - Output Voltage Switch 1 (V)
     OutputVoltageSwitch1 => {vec![0xE4, 0x10], |d| 0.013_458_119 * d},
     /// ISW1 - Output Current Switch 1 (A)
-    OutputCurrentSwitch1 => {vec![0xE4, 0x14], |d| (0.0013484514 * d)},
+    OutputCurrentSwitch1 => {vec![0xE4, 0x14], |d| 0.0013484514 * d},
     /// VSW2 - Output Voltage Switch 2 (V)
     OutputVoltageSwitch2 => {vec![0xE4, 0x20], |d| 0.013_447_282 * d},
     /// ISW2 - Output Current Switch 2 (A)
-    OutputCurrentSwitch2 => {vec![0xE4, 0x24], |d| (0.0013453185 * d)},
+    OutputCurrentSwitch2 => {vec![0xE4, 0x24], |d| 0.0013453185 * d},
     /// VSW3 - Output Voltage Switch 3 (V)
     OutputVoltageSwitch3 => {vec![0xE4, 0x30], |d| 0.008_997_091 * d},
     /// ISW3 - Output Current Switch 3 (A)
-    OutputCurrentSwitch3 => {vec![0xE4, 0x34], |d| (0.0060024010 * d)},
+    OutputCurrentSwitch3 => {vec![0xE4, 0x34], |d| 0.0060024010 * d},
     /// VSW4 - Output Voltage Switch 4 (V)
     OutputVoltageSwitch4 => {vec![0xE4, 0x40], |d| 0.008_991_18 * d},
     /// ISW4 - Output Current Switch 4 (A)
-    OutputCurrentSwitch4 => {vec![0xE4, 0x44], |d| (0.0071794872 * d)},
+    OutputCurrentSwitch4 => {vec![0xE4, 0x44], |d| 0.0071794872 * d},
     /// VSW5 - Output Voltage Switch 5 (V)
     OutputVoltageSwitch5 => {vec![0xE4, 0x50], |d| 0.005_854_875 * d},
     /// ISW5 - Output Current Switch 5 (A)
-    OutputCurrentSwitch5 => {vec![0xE4, 0x54], |d| (0.0013245614 * d)},
+    OutputCurrentSwitch5 => {vec![0xE4, 0x54], |d| 0.0013245614 * d},
     /// VSW6 - Output Voltage Switch 6 (V)
     OutputVoltageSwitch6 => {vec![0xE4, 0x60], |d| 0.005_855_28 * d},
     /// ISW6 - Output Current Switch 6 (A)
-    OutputCurrentSwitch6 => {vec![0xE4, 0x64], |d| (0.0012828721 * d)},
+    OutputCurrentSwitch6 => {vec![0xE4, 0x64], |d| 0.0012828721 * d},
     /// VSW7 - Output Voltage Switch 7 (V)
     OutputVoltageSwitch7 => {vec![0xE4, 0x70], |d| 0.005_852_332 * d},
     /// ISW7 - Output Current Switch 7 (A)
-    OutputCurrentSwitch7 => {vec![0xE4, 0x74], |d| (0.0013731775 * d)},
+    OutputCurrentSwitch7 => {vec![0xE4, 0x74], |d| 0.0013731775 * d},
     /// VSW8 - Output Voltage Switch 8 (V)
     OutputVoltageSwitch8 => {vec![0xE4, 0x80], |d| 0.004_297_822 * d},
     /// ISW8 - Output Current Switch 8 (A)
-    OutputCurrentSwitch8 => {vec![0xE4, 0x84], |d| (0.0012362637 * d)},
+    OutputCurrentSwitch8 => {vec![0xE4, 0x84], |d| 0.0012362637 * d},
     /// VSW9 - Output Voltage Switch 9 (V)
     OutputVoltageSwitch9 => {vec![0xE4, 0x90], |d| 0.004_297_481 * d},
     /// ISW9 - Output Current Switch 9 (A)
-    OutputCurrentSwitch9 => {vec![0xE4, 0x94], |d| (0.0013393179 * d)},
+    OutputCurrentSwitch9 => {vec![0xE4, 0x94], |d| 0.0013393179 * d},
     /// VSW10 - Output Voltage Switch 10 (V)
     OutputVoltageSwitch10 => {vec![0xE4, 0xA0], |d| 0.004_295_835 * d},
     /// ISW10 - Output Current Switch 10 (A)
-    OutputCurrentSwitch10 => {vec![0xE4, 0xA4], |d| (0.0013515982 * d)},
+    OutputCurrentSwitch10 => {vec![0xE4, 0xA4], |d| 0.0013515982 * d},
 
     /// TBRD - Board Temperature (*C)
     BoardTemperature => {vec![0xE3, 0x08], |d|  (0.356_573_44 * d) - 273.884_02},
